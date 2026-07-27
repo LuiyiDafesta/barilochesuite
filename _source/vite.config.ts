1903ac695f5127becac0f2360ebc301e16079c8b
LuiyiDafesta <luiyi@barilochesuite.com>
2026-07-27 14:57:12 -0300

    fix: solucionar pagina en blanco en subrutas admin corrigiendo basepath y reescritura de assets en htaccess

--- _source/vite.config.ts
+++ _source/vite.config.ts
@@ -5,7 +5,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "/barilochesuite/",
   plugins: [
     TanStackRouterVite({
       target: "react",
